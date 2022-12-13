ae1e7d489fa82fbe458eb0fbc4cf855de8e59904
Cleiton Reis <cleitonreis@MacBookPro.local>
2022-12-13 08:14:36 -0300

    Configurando a base.

---
+++ src/routes/index.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import { home, dogs, cats, fishes } from '../controllers/pageController';
+import { search } from '../controllers/searchController';
+
+const router = Router();
+
+router.get('/', home);
+router.get('/dogs', dogs);
+router.get('/cats', cats);
+router.get('/fishes', fishes);
+
+export default router;
